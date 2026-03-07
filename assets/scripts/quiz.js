12b1a553781c9a9f4d59321b6d709dd6257e83f2
CtrlAltKismet <Brookesdet@gmail.com>
2026-03-07 18:16:02 +0000

    Set up quiz for jest tests in quiz.js file.

---
+++ assets/scripts/quiz.js
@@ -0,0 +1,29 @@
+// Quiz test
+
+// Checks if answer selected is correct
+// Returns true if correct, false if not
+
+function checkAnswer (selected, correct) {
+    return selected === correct;
+}
+
+// Returns +1 score if answer is correct, updates the score
+function updateScore (score, isCorrect) {
+    if (isCorrect) {
+        return score + 1;
+    }
+    return score;
+}
+
+// Goes to next question in Quiz
+
+function nextQuestion(currentIndex) {
+    return currentIndex + 1;
+}
+
+module.exports = {
+    checkAnswer,
+    updateScore,
+    nextQuestion
+};
+
