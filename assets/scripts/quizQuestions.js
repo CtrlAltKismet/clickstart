23b03e6f446f9033f01bf7e4e8e2f102e823909b
CtrlAltKismet <Brookesdet@gmail.com>
2026-03-07 18:49:42 +0000

    Set up first 3 questions for quiz.

---
+++ assets/scripts/quizQuestions.js
@@ -0,0 +1,34 @@
+const quizQuestions = [
+    {
+        question: "Which of the following is the most secure password?",
+        answers: [
+            "password123",
+            "MyDogMax",
+            "W1nt3r_tru5t_0909!",
+            "12345678"
+        ],
+        correct: "W1nt3r_tru5t_0909!"
+    },
+
+    {
+        question: "A secure website address often starts with:",
+        answers: [
+            "http://",
+            "ftp://",
+            "https://",
+            "www."
+        ],
+        correct: "https://"
+    },
+
+    {
+        question: "What is the main purpose of an Antivirus program?",
+        answers: [
+            "To speed up your internet connection",
+            "To protect your computer from malicious software",
+            "To manage files and folders",
+            "To block websites"
+        ],
+        correct: "To protect your computer from malicious software"
+    }
+];
